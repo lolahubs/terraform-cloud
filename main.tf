e76ebd11772a94a3cc0f7aa2f8c426be291bb415
damihubs <damiakinkuolie@gmail.com>
2024-11-20 19:26:14 -0600

    Create main.tf
    "Add main.tf"

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source              = "./modules/resource_group"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
+module "storage_account" {
+  source               = "./modules/storage_account"
+  storage_account_name = var.storage_account_name
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = var.location
+}
