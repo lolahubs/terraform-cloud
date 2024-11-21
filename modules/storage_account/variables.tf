25e2850ccc1aa3dbe04c8ef96c1fc394fba08ebf
lolahubs <159863404+lolahubs@users.noreply.github.com>
2024-11-20 21:31:00 -0600

    Update variables.tf

--- modules/storage_account/variables.tf
+++ modules/storage_account/variables.tf
@@ -1,6 +1,7 @@
 variable "storage_account_name" {
   description = "Name of the Azure Storage Account"
   type        = string
+  default     = "diveronegreat12"
 }
 
 variable "resource_group_name" {
