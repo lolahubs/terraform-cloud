d63e6c6cab2166efcd78367e5e57c60d5b3a42d5
damihubs <damiakinkuolie@gmail.com>
2024-11-20 19:32:31 -0600

    Create variables.tf
    add modul storage variables.tf

---
+++ modules/storage_account/variables.tf
@@ -0,0 +1,14 @@
+variable "storage_account_name" {
+  description = "Name of the Azure Storage Account"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure location"
+  type        = string
+}
